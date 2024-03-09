14dd8706fd79f51cce536df1fc80f528193caab9
Gargamel-l <belyton2002@yandex.ru>
2024-03-09 13:43:11 +0300

    Add project files.

---
+++ Recipes.py
@@ -0,0 +1,27 @@
+﻿import pandas as pd
+
+# Список простых рецептов для обучения
+data = [
+    {"ingredients": "томаты, огурцы, оливковое масло, соль", "recipe": "Салат из томатов и огурцов"},
+    {"ingredients": "куриная грудка, соевый соус, мед, чеснок", "recipe": "Маринованная курица"},
+    {"ingredients": "макароны, соль, вода, сыр пармезан, черный перец", "recipe": "Макароны с сыром"},
+    {"ingredients": "лосось, соль, лимон, оливковое масло", "recipe": "Запеченный лосось"},
+    {"ingredients": "авокадо, лимонный сок, соль, перец", "recipe": "Гуакамоле"},
+    {"ingredients": "яйца, соль, молоко, сливочное масло", "recipe": "Омлет"},
+    {"ingredients": "бананы, молоко, мед, лед", "recipe": "Банановый смузи"},
+    {"ingredients": "гречка, вода, соль", "recipe": "Вареная гречка"},
+    {"ingredients": "шпинат, чеснок, оливковое масло, соль", "recipe": "Жареный шпинат"},
+    {"ingredients": "курица, карри, кокосовое молоко, лук", "recipe": "Курица карри"},
+    {"ingredients": "хлеб, майонез", "recipe": "бедный бутерброд"},
+    {"ingredients": "хлеб, калбоса, сыр", "recipe": "обыкновенный бутерброд"}
+]
+
+# Создание DataFrame
+recipes_df = pd.DataFrame(data)
+
+# Сохранение в CSV-файл
+recipes_df.to_csv('D:/programming/VisualStudion/workZone/RecipyModel/recipes.csv', index=False)
+
+# Возвращаем путь к файлу
+'D:/programming/VisualStudion/workZone/RecipyModel/recipes.csv'
+
---
+++ RecipyModel.py
@@ -0,0 +1,30 @@
+﻿import pandas as pd
+
+# Загрузка данных
+data = pd.read_csv('recipes.csv')
+
+# Функция для поиска рецептов
+def find_recipes(ingredients):
+    ingredients_set = set(ingredients.split(', '))
+    possible_recipes = []
+    
+    for index, row in data.iterrows():
+        recipe_ingredients = set(row['ingredients'].split(', '))
+        # Если все введенные ингредиенты содержатся в рецепте, добавляем его в список
+        if ingredients_set.issuperset(recipe_ingredients):
+            possible_recipes.append(row['recipe'])
+            
+    return possible_recipes
+
+# Интерактивный ввод пользователя
+print("Введите ингредиенты, разделяя их запятой (например, 'томаты, огурцы, сыр'): ")
+user_ingredients = input()
+
+# Поиск и вывод рецептов
+possible_recipes = find_recipes(user_ingredients)
+if possible_recipes:
+    print("Из этих ингредиентов можно приготовить следующие блюда:")
+    for recipe in possible_recipes:
+        print(f"- {recipe}")
+else:
+    print("Из данных ингредиентов нельзя приготовить ни одного из известных рецептов.")
